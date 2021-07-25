eb9fee82b676c0f8c262a366145dc19bec642756
ShayneWilliams <65196635+ShayneWilliams@users.noreply.github.com>
2021-07-24 20:43:32 -0700

    home page
    The home page is looking pretty good now at 1440 pixels. It just needs that gray irregular polygon background, to be more responsive, and to have the navlinks finalized.

--- designo/components/OrangeCard.js
+++ designo/components/OrangeCard.js
@@ -3,14 +3,18 @@ import styles from '../styles/OrangeCard.module.css'
 export default function OrangeCard() {
     return (
         <div className ={styles.container}>
-            <div className ={styles['container__text']}>
+            <div className ={styles['container__left']}>
             <h2>Let’s talk about <br/>your project</h2>
             <p>Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.</p>
             </div>
 
-            <div>
+            <div className = {styles['container__right']}>
                 <button>GET IN TOUCH</button>
             </div>
+            {/* <div className={styles['test']}>
+
+            <img src='/assets/shared/desktop/bg-pattern-three-circles.svg'/>
+            </div> */}
         </div>
     )
 }
---
+++ designo/pages/web-design.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function WebDesign() {
+    return (
+        <div>
+            <h1>WEB DESIGN</h1>
+        </div>
+    )
+}
